d05394ed6845da9e3793ecc6ac32c3741f979b10
Strangexz <ja_figueroa@outlook.es>
2024-08-12 12:39:45 -0600

    feat: agregado archivo main

---
+++ src/main/java/hn/com/jf/CursoSpringBootAz.java
@@ -0,0 +1,12 @@
+package hn.com.jf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootAz {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootAz.class, args);
+	}
+}
