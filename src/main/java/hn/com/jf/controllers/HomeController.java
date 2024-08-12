ab62e01ca1f1c2be2b62fb98dbe4627346459599
Strangexz <ja_figueroa@outlook.es>
2024-08-12 16:16:55 -0600

    feat: Controlador de redireccionamiento

---
+++ src/main/java/hn/com/jf/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package hn.com.jf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping({ "", "/", "/home" })
+	public String home() {
+		return "redirect/list";
+//		return "forward/list";
+	}
+
+}
