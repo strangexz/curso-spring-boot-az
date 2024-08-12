a0f7417ef045bdf628f22f24b4b9495e97f9c256
Strangexz <ja_figueroa@outlook.es>
2024-08-12 12:41:13 -0600

    feat: archivo de configuración de properties personalizado

---
+++ src/main/java/hn/com/jf/SecurityConfig.java
@@ -0,0 +1,11 @@
+package hn.com.jf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({ @PropertySource("classpath:values.properties") })
+public class SecurityConfig {
+
+}
